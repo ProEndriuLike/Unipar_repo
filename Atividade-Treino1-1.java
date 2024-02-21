2aa886bd6098cd09fc2f75b40cc317e23c2297fb
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-02-20 22:22:48 -0300

    Create Atividade-Treino1-1.java
    Atividade de treinamento em java
    
    Sobre a atividade: 10 tipos de váriaveis.

---
+++ Atividade-Treino1-1.java
@@ -0,0 +1,25 @@
+class Atividades {
+    public static void main(String[] args){
+        
+        int idade = 19;
+        
+        double peso = 85.20;
+        
+        float preco = 3.99f;
+        
+        long codigoDeBarras = 789651599;
+        
+        short ano = 2024;
+        
+        byte posicao = 1;
+        
+        char sexo = 'M';
+        
+        boolean identidade = true;
+      
+        float media = 7.5f;
+        
+        char tipoSanguineo = 'O';
+      
+    }
+}
