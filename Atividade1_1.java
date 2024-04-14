ac2f6b6245055c8a3cee55de29b505081ec9cea3
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-14 16:37:31 -0300

    Create Atividade1_1.java

---
+++ Atividade1_1.java
@@ -0,0 +1,14 @@
+class Atividade1_1 {
+    public static void main(String[] args) {        
+        int idade = 19;
+        double peso = 20.54;
+        float nota = 6.8f;
+        long data = 20022024;
+        short ano = 2024;
+        byte unidade = 2;       
+        char sexo = 'M';      
+        boolean aprovacao = true;       
+        int semestre = 2;        
+        double altura = 1.75;
+    }
+}
