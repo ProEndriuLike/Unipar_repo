62f86f499694a4140c415111e6609bfd9cba2838
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-14 16:57:16 -0300

    Create Atividade1_7.java

---
+++ Atividade1_7.java
@@ -0,0 +1,14 @@
+public class Atividade1_7 {
+    public static void main(String[] args) {
+        String nomeProduto = "Fone de Ouvido";
+        String marcaProduto = "Samsung";
+        double precoProduto = 29.99;
+        int quantidadeProduto = 10;
+
+        System.out.println("Dados do Produto:");
+        System.out.println("Nome: " + nomeProduto);
+        System.out.println("Marca: " + marcaProduto);
+        System.out.println("Preço: $" + precoProduto);
+        System.out.println("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
