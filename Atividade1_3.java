7a0893d001c0eb2cf6c86c8156ff5ca3bfc60c64
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-14 16:39:32 -0300

    Create Atividade1_3.java

---
+++ Atividade1_3.java
@@ -0,0 +1,11 @@
+public class Atividade1_3 {
+    public static void main(String[] args) {
+        String nomeDoAluno = "Endriu";
+        int idadeDoAluno = 19;
+        double notaDaProva1 = 9;
+        double notaDaProva2 = 9.5;
+        double mediaFinal = (notaDaProva1 + notaDaProva2) / 2;
+
+        System.out.println("A média final do aluno " + nomeDoAluno + " é: " + mediaFinal);
+    }
+}
