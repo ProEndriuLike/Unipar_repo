7061f1806f3f03b9ceb46a24707b1589eb1ebebf
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-14 16:38:22 -0300

    Create Atividade1_2.java

---
+++ Atividade1_2.java
@@ -0,0 +1,14 @@
+public class Atividade1_2 {
+    public static void main(String[] args) {
+        String nomeProduto;
+        double precoProduto;
+        String marcaDoProduto;
+        int quantidadeProduto;
+        String descricaoProduto;
+        String nomeDoProduto;
+        int numeroProduto;
+        double precoDoProduto;
+        int estoqueDisponivel;
+        int totaldeProdutos;
+    }
+}
