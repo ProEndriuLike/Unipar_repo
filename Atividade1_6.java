cf8b543ac29410c803e217924721fa8b275244e0
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-14 16:56:34 -0300

    Create Atividade1_6.java

---
+++ Atividade1_6.java
@@ -0,0 +1,12 @@
+public class Atividade1_6 {
+    public static void main(String[] args) {
+        int numeroProduto = 123;
+        double precoProduto = 25;
+        String descricaoProduto = "Fone de ouvido Bluetooth";
+
+        System.out.println("Dados do Produto:");
+        System.out.println("Número: " + numeroProduto);
+        System.out.println("Preço: $" + precoProduto);
+        System.out.println("Descrição: " + descricaoProduto);
+    }
+}
