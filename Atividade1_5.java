dd6ece9bf21eb093b327678993dfdba70dbf5c98
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-04-14 16:55:52 -0300

    Create Atividade1_5.java

---
+++ Atividade1_5.java
@@ -0,0 +1,34 @@
+public class Atividade1_5 {
+    public static void main(String[] args){
+        String nomeSmartphone = "Redmi 12";
+        String nomeLaptop = "Aspire 5";
+        String nomeFoneBluetooth = "AirPods Pro";
+        String marcaSmartphone = "Xiaomi";
+        String marcaLaptop = "Acer";
+        String marcaFoneBluetooth = "Apple";
+        double precoSmartphone = 1200;
+        double precoLaptop = 3000;
+        double precoFoneBluetooth = 229.99;
+        int estoqueSmartphone = 24;
+        int estoqueLaptop = 17;
+        int estoqueFoneBluetooth = 36;
+
+        System.out.println("Dados do Smartphone:");
+        System.out.println("Nome: " + nomeSmartphone);
+        System.out.println("Marca: " + marcaSmartphone);
+        System.out.println("Preço: " + precoSmartphone);
+        System.out.println("Quantidade em estoque: " + estoqueSmartphone);
+        System.out.println("");
+        System.out.println("Dados do Laptop:");
+        System.out.println("Nome: " + nomeLaptop);
+        System.out.println("Marca: " + marcaLaptop);
+        System.out.println("Preço: " + precoLaptop);
+        System.out.println("Quandidade em estoque: " + estoqueLaptop);
+        System.out.println("");
+        System.out.println("Dados do Fone Bluetooth:");
+        System.out.println("Nome: " + nomeFoneBluetooth);
+        System.out.println("Marca: " + marcaFoneBluetooth);
+        System.out.println("Preço: " + precoFoneBluetooth);
+        System.out.println("Quantidade em estoque: " + estoqueFoneBluetooth);
+    }
+}
