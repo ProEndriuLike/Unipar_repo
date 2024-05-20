d5db272d18876775ccd2ddff4d284c1356104e18
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-05-20 19:12:47 -0300

    Add files via upload

---
+++ ProjetoContaBancaria/src/main/java/br/unipar/ContaPoupanca.java
@@ -0,0 +1,15 @@
+package br.unipar;
+
+public class ContaPoupanca extends HerancaContaBancaria{
+    @Override
+    public boolean saque(Double valor) {
+        double verificar = saldo + 5;
+        if((saldo)>= valor){
+            saldo -= valor;
+            saldo -= 5;
+
+            return true;
+        }
+        return false;
+    }
+}
---
+++ ProjetoContaBancaria/src/test/java/ContaBancariaTeste.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.*;
+import br.unipar.ContaCorrente;
+import org.junit.jupiter.api.Test;
+
+public class ContaBancariaTeste {
+
+    @Test
+    public void testeDepositoContaCorrente(){
+
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(5000d);
+
+        assertEquals(conta.obterSaldo(), 5000);
+    }
+    @Test
+    public void testSaqueContaCorrente(){
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(3000d);
+        boolean validacao = conta.saque(1500d);
+
+        assertTrue(validacao);
+        assertEquals(conta.obterSaldo(), 1500);
+    }
+    @Test
+    public void testSaqueMaiorContaCorrente(){
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(3000d);
+        boolean validacao = conta.saque(1500d);
+
+        assertFalse(validacao);
+        assertEquals(conta.obterSaldo(), 1500);
+    }
+
+    @Test
+    public void testeDepositoContaPoupanca(){
+
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(5000d);
+
+        assertEquals(conta.obterSaldo(), 5000);
+    }
+    @Test
+    public void testSaqueContaPoupanca(){
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(3000d);
+        boolean validacao = conta.saque(1500d);
+
+        assertTrue(validacao);
+        assertEquals(conta.obterSaldo(), 1495);
+    }
+    @Test
+    public void testSaqueMaiorContaPoupanca(){
+        ContaCorrente conta = new ContaCorrente();
+        conta.depositar(3d);
+        boolean validacao = conta.saque(5d);
+
+        assertFalse(validacao);
+        assertEquals(conta.obterSaldo(), 3);
+    }
+}
