a37fe1e7097285222314f75311bb345f39a44a37
ProEndriuLike <160682681+ProEndriuLike@users.noreply.github.com>
2024-05-20 19:15:30 -0300

    Update ContaPoupanca.java

--- ProjetoContaBancaria/src/main/java/br/unipar/ContaPoupanca.java
+++ ProjetoContaBancaria/src/main/java/br/unipar/ContaPoupanca.java
@@ -4,7 +4,7 @@ public class ContaPoupanca extends HerancaContaBancaria{
     @Override
     public boolean saque(Double valor) {
         double verificar = saldo + 5;
-        if((saldo)>= valor){
+        if(verificar >= valor){
             saldo -= valor;
             saldo -= 5;
 
